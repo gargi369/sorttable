25b726655e0413b2c30d78db302337939049b0d3
dbrink <dbrink@gmail.com>
2011-06-01 14:23:51 -0400

    initial commit

---
+++ WebContent/js/jquery.sorttable.js
@@ -0,0 +1,181 @@
+﻿/*!
+* sorttable
+*
+* @Version 1.0.0
+*
+* Copyright (c) 2011, Andres Koetter akottr@gmail.com, David Brink dbrink@gmail.com
+* Dual licensed under the MIT (MIT-LICENSE.txt)
+* and GPL (GPL-LICENSE.txt) licenses.
+*
+* Inspired by the the dragtable from Dan Vanderkam (danvk.org/dragtable/)
+* Thanks to the jquery and jqueryui comitters
+* 
+* Any comment, bug report, feature-request is welcome
+* Feel free to contact me.
+*/
+(function ($) {
+    $.widget("akottr.sorttable", $.ui.sortable, {
+        widgetEventPrefix: "sorttable",
+        options: {
+            helper: "table"
+        },
+        _table: null,
+        _startIndex: 0,
+        _endIndex: 0,
+        _currentItemWidth: 0,
+        _createHelper: function (event) {
+            var o = this.options;
+
+            if (o.helper == 'table') {
+                // if using 'table' helper
+                return this._createHelperTable(event, this.currentItem);
+            }
+            else {
+                // use default helper method
+                return $.ui.sortable.prototype._createHelper.apply(this, [event]);
+            }
+        },
+        _createHelperTable: function (e, ui) {
+            var th = ui.is('th') || ui.is('td') ? ui : ui.parents('th:first,td:first');
+            var tr = th.parents('tr:first');
+            var table = tr.parents('table:first');
+            var thWidth = th.innerWidth() - parseInt(th.css('paddingLeft') || 0, 10) - parseInt(th.css('paddingRight') || 0, 10);
+            var trHeight = tr.innerHeight() - parseInt(tr.css('paddingTop') || 0, 10) - parseInt(tr.css('paddingBottom') || 0, 10);
+
+            // save sizes because <ie9 reports wrong values when display:none
+            this._currentItemWidth = thWidth;
+
+            var thClone = th.clone();
+            thClone.width(thWidth);
+            var trClone = tr.clone().empty();
+            trClone.height(trHeight);
+            var tableClone = table.clone().empty();
+            tableClone.css('position', 'absolute');
+            tableClone.css('width', 'auto');
+            tableClone.css('height', 'auto');
+            tableClone.attr('id', '');
+            thClone.appendTo(trClone.appendTo(tableClone));
+            tableClone.find().each(function () {
+                this.attr('id', ''); // clear ids on cloned table
+            });
+            table.before(tableClone);
+            return tableClone;
+        },
+        _createPlaceholder: function (that) {
+            var self = that || this, o = self.options;
+
+            // call base method
+            $.ui.sortable.prototype._createPlaceholder.apply(this, arguments);
+
+            if ($.browser.webkit) {
+                // chrome (maybe all webkit) gets crazy when hiding a table-cell
+                this._reAttach(this.element);
+            }
+
+            if (o.helper == 'table') {
+                // force width as border will prevent width from setting even though cell has no explicit width
+                self.placeholder.width(self._currentItemWidth);
+            }
+        },
+        _swapNodes: function (a, b) {
+            if (a && b) {
+                var aparent = a.parentNode;
+                var asibling = a.nextSibling === b ? a : a.nextSibling;
+                b.parentNode.insertBefore(a, b);
+                aparent.insertBefore(b, asibling);
+            }
+        },
+        // bubble the moved col left or right
+        _bubbleCols: function () {
+            var from = this._startIndex;
+            var to = this._endIndex;
+            /* Find children thead and tbody.
+            * Only to process the immediate tr-children. Bugfix for inner tables
+            */
+            var trs = this._table.children().find('> tr:not(.ui-sortable)');
+            if (from < to) {
+                for (var i = from; i < to; i++) {
+                    var row1 = trs.find('>td:nth-child(' + i + '),>th:nth-child(' + i + ')');
+                    var row2 = trs.find('>td:nth-child(' + (i + 1) + '),>th:nth-child(' + (i + 1) + ')')
+                    for (var j = 0; j < row1.length; j++) {
+                        this._swapNodes(row1[j], row2[j]);
+                    }
+                }
+            }
+            else {
+                for (var i = from; i > to; i--) {
+                    var row1 = trs.find('>td:nth-child(' + i + '),>th:nth-child(' + i + ')');
+                    var row2 = trs.find('>td:nth-child(' + (i - 1) + '),>th:nth-child(' + (i - 1) + ')')
+                    for (var j = 0; j < row1.length; j++) {
+                        this._swapNodes(row1[j], row2[j]);
+                    }
+                }
+            }
+        },
+        _rearrangeTableBackroundProcessing: function () {
+            var _this = this;
+            return function () {
+                _this._bubbleCols();
+            };
+        },
+        _reAttach: function (el) {
+            var unwrap = el[0], parent = unwrap.parentNode, next = unwrap.nextSibling;
+            el.detach();
+            if (next) {
+                parent.insertBefore(unwrap, next);
+            }
+            else {
+                parent.appendChild(unwrap);
+            }
+        },
+        _sortStart: function () {
+            var _this = this;
+            return function (e, ui) {
+                _this._startIndex = ui.item.prevAll().length + 1;
+            }
+        },
+        _sortUpdate: function () {
+            var _this = this;
+            return function (e, ui) {
+                _this._endIndex = ui.item.prevAll().length + 1;
+                if (_this._startIndex != _this._endIndex) {
+                    // do reorganisation asynchronous
+                    // for chrome a little bit more than 1 ms because we want to force a rerender
+                    setTimeout(_this._rearrangeTableBackroundProcessing(), 50);
+                }
+            }
+        },
+        _create: function () {
+            var el = this.element;
+            if (el.is('table')) {
+                this._table = el;
+                // reset element to first tr
+                el = this.element = el.find('tr:first');
+            }
+            else {
+                // find parent table
+                this._table = el.parents('table:first');
+            }
+
+            this._startIndex = 0;
+            this._endIndex = 0;
+
+            // bind to start and update
+            el.bind('sorttablestart', this._sortStart());
+            el.bind('sorttableupdate', this._sortUpdate());
+
+            $.ui.sortable.prototype._create.apply(this);
+
+            var items = this.items;
+            var widths = [];
+            for (i = 0; i < items.length; i++) {
+                var item = $(items[i].item[0]);
+                widths[i] = item.innerWidth() - parseInt(item.css('paddingLeft') || 0, 10) - parseInt(item.css('paddingRight') || 0, 10);
+            }
+            for (i = 0; i < items.length; i++) {
+                var item = $(items[i].item[0]);
+                item.width(widths[i]);
+            }
+        }
+    });
+})(jQuery);
