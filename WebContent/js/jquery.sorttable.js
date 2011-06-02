2341292646ef90a877f713f888be55f739580cae
dbrink <dbrink@gmail.com>
2011-06-02 11:37:29 -0400

    updated header

--- WebContent/js/jquery.sorttable.js
+++ WebContent/js/jquery.sorttable.js
@@ -3,7 +3,8 @@
 *
 * @Version 1.0.1
 *
-* Copyright (c) 2011, Andres Koetter akottr@gmail.com, David Brink dbrink@gmail.com
+* Copyright (c) 2011, David Brink dbrink@gmail.com
+* Copyright (c) 2010, Andres Koetter akottr@gmail.com
 * Dual licensed under the MIT (MIT-LICENSE.txt)
 * and GPL (GPL-LICENSE.txt) licenses.
 *
